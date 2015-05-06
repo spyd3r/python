bad428a30aa2f7af9f21118078f88c8e00f68599
spyd3r <scott.foger@gmail.com>
2015-05-05 22:05:15 -0600

    Create fib() and fib2() functions.

---
+++ fibonacci_series.py
@@ -0,0 +1,24 @@
+__author__ = 'Scott'
+
+
+def fib(n):     #write Fibonacci series up to n
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+        print()
+
+
+def fib2(n):
+    """Return a list containing the Fibonacci series up to n."""
+    result = []
+    a, b = 0,1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+if __name__== "__main__":
+    fib(2000)
+    print(fib2(50000))
